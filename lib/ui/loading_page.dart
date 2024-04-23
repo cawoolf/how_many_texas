a18b58af02e0f91de42d6b8ef2589d3e33b4653d
cawoolf <destellodev@gmail.com>
2024-04-22 19:46:15 -0600

    loading page progress

--- lib/ui/loading_page.dart
+++ lib/ui/loading_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:how_many_texas/ui/response_page.dart'; // Import async library for Future and Timer
+import 'package:how_many_texas/ui/response_page.dart';
+import 'package:how_many_texas/utils/colors.dart'; // Import async library for Future and Timer
+
 class LoadingPage extends StatelessWidget {
   const LoadingPage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     // Start a timer that triggers after 2 seconds
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 3), () {
       // Navigate to the ResponsePage after the delay and replace the current route
       // Prevents multiple instances of the HomePage being pushed onto the stack by the ResponsePage
       Navigator.pushReplacement(
@@ -17,9 +18,63 @@ class LoadingPage extends StatelessWidget {
       );
     });
 
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(), // Show a loading indicator in the center
+    return SafeArea(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildHeaderText(),
+        _showCowGraphic(),
+        _buildFooterText(),
+      ],
+    ));
+  }
+
+  Expanded _buildFooterText() {
+    return Expanded(
+      child: Container(
+        color: AppColors.backgroundColor,
+        child: Center(
+          child: Text(
+            'Widget 3',
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Flexible _showCowGraphic() {
+    return Flexible(
+      flex: 3,
+      child: Stack(
+        children: [
+          // Background Color
+          Container(
+            color: AppColors.backgroundColor, // Adjust opacity as needed
+          ),
+
+          // Background Image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/cow_1_trimmed.png',
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Expanded _buildHeaderText() {
+    return Expanded(
+      child: Container(
+        color: AppColors.backgroundColor,
+        child: Center(
+          child: Text(
+            'Widget 1',
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // static const Color primaryColor = Color(0xFF009688);
+  // static const Color secondaryColor = Color(0xFF3F51B5);
+  // static const Color accentColor = Color(0xFFFFC107);
+
+  static const Color backgroundColor = Color(0xFFD4E7EB);
+}
