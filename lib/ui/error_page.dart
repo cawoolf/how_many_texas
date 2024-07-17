05a92506e268fbb23c8a63a02429f04b4c4d19ca
cawoolf <destellodev@gmail.com>
2024-07-16 19:57:28 -0600

    updating error page

---
+++ lib/ui/error_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:how_many_texas/constants/colors.dart';
+import 'package:how_many_texas/constants/text_styles.dart';
+
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Not UI
+    // _delayedNavToResponsePage(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: _backgroundColor(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildHeaderText(),
+              _showCowGraphic(),
+              _buildFooterText()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildHeaderText() {
+    return Expanded(
+      child: Center(
+        child: Text(
+          'Woops..',
+          style: AppTextStyles.loadingPageHeaderTextStyle,
+        ),
+      ),
+    );
+  }
+
+  Flexible _showCowGraphic() {
+    return Flexible(
+      flex: 3,
+      child: Stack(
+        children: [
+          // Background Color
+          // Background Image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/cow_1_trimmed.png',
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Expanded _buildFooterText() {
+    return Expanded(
+      child: Center(
+        child: Text(
+          'Try that again partner..',
+          style: AppTextStyles.loadingPageFooterTextStyle,
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _backgroundColor() {
+    return const BoxDecoration(color: AppColors.backgroundColor);
+  }
+}
