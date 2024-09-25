d8dece8d6443a0e7cf6121fa232f943c4050c120
cawoolf <destellodev@gmail.com>
2024-09-24 19:25:39 -0600

    uhhh

--- lib/purchase_test.dart
+++ lib/purchase_test.dart
@@ -1,10 +1,75 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class PurchaseTest extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+
+class PurchaseTest extends StatefulWidget {
   const PurchaseTest({super.key});
+
+  @override
+  State<PurchaseTest> createState() => _PurchaseTestState();
+}
+
+class _PurchaseTestState extends State<PurchaseTest> {
+  late StreamSubscription<List<PurchaseDetails>> _subscription;
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
+
+  @override
+  void initState() {
+    final Stream<List<PurchaseDetails>> purchaseUpdated =
+        InAppPurchase.instance.purchaseStream;
+
+    _subscription = purchaseUpdated.listen((purchaseDetailsList) {
+      _listenToPurchaseUpdated(purchaseDetailsList);
+    }, onDone: () {
+      _subscription.cancel();
+    }, onError: (error) {
+      // handle error here.
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
 }
+
+void _listenToPurchaseUpdated(List<PurchaseDetails> purchaseDetailsList) {
+  purchaseDetailsList.forEach((PurchaseDetails purchaseDetails) async {
+    if (purchaseDetails.status == PurchaseStatus.pending) {
+      _showPendingUI();
+    } else {
+      if (purchaseDetails.status == PurchaseStatus.error) {
+        _handleError(purchaseDetails.error!);
+      } else if (purchaseDetails.status == PurchaseStatus.purchased ||
+          purchaseDetails.status == PurchaseStatus.restored) {
+        bool valid = await _verifyPurchase(purchaseDetails);
+        if (valid) {
+          _deliverProduct(purchaseDetails);
+        } else {
+          _handleInvalidPurchase(purchaseDetails);
+        }
+      }
+      if (purchaseDetails.pendingCompletePurchase) {
+        await InAppPurchase.instance
+            .completePurchase(purchaseDetails);
+      }
+    }
+  });
+  
+  
+}
+
+void _handleError(IAPError iapError) {
+}
+
+void _showPendingUI() {
+}
+
